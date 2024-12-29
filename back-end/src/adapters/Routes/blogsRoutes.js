20139181c08d8c6230a4e2724023f8bba0c61a45
Mesay-AK <mesabe77@gmail.com>
2024-12-30 02:21:09 +0300

    changes

--- back-end/src/adapters/Routes/blogsRoutes.js
+++ back-end/src/adapters/Routes/blogsRoutes.js
@@ -12,15 +12,15 @@ const blogRepository = new BlogRepository();
 const blogUseCase = new BlogUseCase(blogRepository);
 const blogController = new BlogController(blogUseCase);
 
-router.post('/create', (req, res) => blogController.createBlog(req, res));
-router.put('/edit/:blogId', (req, res) => blogController.updateBlog(req, res));
-router.delete('/delete/:blogId', (req, res) => blogController.deleteBlog(req, res));
+router.post('/create', authMiddleware, adminOnlyMiddleware, (req, res) => blogController.createBlog(req, res));
+router.put('/edit/:blogId',authMiddleware, adminOnlyMiddleware, (req, res) => blogController.updateBlog(req, res));
+router.delete('/delete/:blogId',authMiddleware, adminOnlyMiddleware, (req, res) => blogController.deleteBlog(req, res));
 router.get('/get/:blogId', (req, res) => blogController.getBlog(req, res));
 
 
 router.get('/get', (req, res) => blogController.listBlogs(req, res));
 router.post('/addComment', (req, res) => blogController.addComment(req, res));
-router.delete('/deleteComment', (req, res) => blogController.removeComment(req, res))
+router.delete('/deleteComment',authMiddleware, adminOnlyMiddleware, (req, res) => blogController.removeComment(req, res))
 router.get('/getComments'), (req, res) => blogController.getComments(req, res);
 router.post('/like/:blogId'),(req, res) => blogController.addLike(req, res);
 router.post('/dislike/:blogId'),(req, res) => blogController.addLike(req, res);
