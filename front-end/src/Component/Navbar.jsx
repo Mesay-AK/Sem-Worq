474b82ed02820e3f85520a5974025e80b986eb0a
Hilina Nigatu <hnigatu06@gmail.com>
2024-12-29 13:37:25 -0800

    adding navigation bar

---
+++ front-end/src/Component/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="w-full h-20 bg-[#0f0e0e] flex items-center justify-between px-8">
+      {/* Logo - Positioned on the left */}
+      <div className="flex items-center">
+        <img
+          src="/images/Logo.png" // Update this path based on your logo location
+          alt="Logo"
+          className="w-24" // Adjust the width of the logo as needed
+        />
+      </div>
+
+      {/* Navigation Links - Positioned on the right */}
+      <nav>
+        <ul className="flex space-x-8">
+          {["Home", "Service", "Blog", "Portfolio", "Contact"].map((item) => (
+            <li key={item} className="relative">
+              <a
+                href="#"
+                className="text-[#e7b308] text-lg font-itim no-underline relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[3px] after:w-0 after:bg-[#e7be04] after:transition-all after:duration-400 hover:after:w-full"
+              >
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
